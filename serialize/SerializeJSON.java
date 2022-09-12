3950b1920a8dcedaeb92babb8efebade35dd6594
Lstamp <lozenkrantz@gmail.com>
2022-09-13 01:37:40 +0900

    create classes

---
+++ serialize/SerializeJSON.java
@@ -0,0 +1,18 @@
+package serialize;
+
+public class SerializeJSON {
+    public static void main(String[] args) {
+        //객체 생성
+        Member member = new Member("테스트", "test@test.com", 25);
+
+        // member객체를 json으로 변환 
+        String json = String.format(
+            "{\"name\":\"%s\",\"email\":\"%s\",\"age\":%d}",
+            member.getName(), member.getEmail(), member.getAge());
+
+        
+        byte[] jsonByte = json.getBytes();
+        System.out.println(new String(jsonByte));
+        System.out.printf("json (byte size = %s)\n", jsonByte.length);
+    }
+}
